e51e6bcb999ba606544f4d90dbcd79297c87eec5
Matthew Somerville <matthew-github@dracos.co.uk>
2019-05-22 10:48:40 +0100

    Add line to st.h to include header in wrapper code
    Recompile with SWIG.
    Fixes https://rt.cpan.org/Ticket/Display.html?id=121151

--- st.h
+++ st.h
@@ -1,3 +1,8 @@
-/* swig -perl -module Ediff ~/courses/st.h */
+/* swig -perl -module String::Ediff st.h */
+
+%module st
+%{
+char *ediff(char *s1, char *s2);
+%}
 
 char *ediff(char *s1, char *s2);
